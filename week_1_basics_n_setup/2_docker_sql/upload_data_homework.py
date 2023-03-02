7e6eeb6b2c98a71fda2babad3070bf3ceef1d3cf
Olliewan <olliewan@gmail.com>
2023-03-02 09:36:48 +0000

    upload_data

---
+++ week_1_basics_n_setup/2_docker_sql/upload_data_homework.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+import argparse
+import pandas as pd
+import time
+from sqlalchemy import create_engine
+import os
+
+def main(params):
+    
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_data_source = 'output.csv.gz'
+    else:
+        csv_data_source = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_data_source}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine.connect()
+
+    # Check the schema for this data set
+    df = pd.read_csv(csv_data_source, sep='\t', nrows=100)
+    print('Show the table schema before transformation: \n',pd.io.sql.get_schema(df, name = table_name, con=engine))
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    print('Show the table schema after transformation: \n',pd.io.sql.get_schema(df, name = table_name, con=engine))
+
+    # Create table 
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df_iter = pd.read_csv(csv_data_source, sep='\t', dtype={'store_and_fwd_flag': str}, iterator=True, chunksize=500000)
+    loop = True
+    while loop: 
+        try:
+            t_start = time.time()
+            df = next(df_iter)
+            df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+            df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+            t_end = time.time()
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+        except StopIteration:
+            loop = False
+            print('Iteration is END!!!')
+
+    '''# TEST database
+    query = f"""
+    SELECT * FROM {table_name} LIMIT 10
+    """
+    pd.read_sql(query, con=engine)
+    '''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest parquet data to Postgres')
+    # user, password, host, port, database name, table name, url of the parquet file
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port name for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the parquet file')
+    
+    args = parser.parse_args()
+    main(args)
